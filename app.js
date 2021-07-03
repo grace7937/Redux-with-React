f8d95dca622e37514e3ccacf150aa0d1558b172b
grace7937 <grace7937@github.com>
2021-07-03 17:52:57 +0900

    Add app js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Root</h1>
+      <AddNumberRoot></AddNumberRoot>
+      <DisplayNumberRoot></DisplayNumberRoot>
+    </div>
+  )
+}
